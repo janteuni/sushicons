c1c94b8c687fee791df1a912b8bd63b64e5821b0
janteuni <janteuni@student.42.fr>
2015-04-03 16:13:37 +0200

    feat(gulp): add icon-list

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 var gulp = require('gulp');
 var iconfont = require('gulp-iconfont');
 var iconfontCss = require('gulp-iconfont-css');
+var fs = require('fs');
 
 var fontName = 'sushicons';
 
-gulp.task('iconfont', function(){
+gulp.task('iconfont', function(done){
+
+  var icons;
+
   gulp.src(['icons/*.svg'])
     .pipe(iconfontCss({
       fontName: fontName,
@@ -15,6 +19,14 @@ gulp.task('iconfont', function(){
     .pipe(iconfont({
       fontName: fontName
      }))
-    .pipe(gulp.dest('dist/font'));
+	.on('codepoints', function(codepoints, options) {
+		icons = codepoints.map(function(el) { return 'sushicon-' + el.name; });
+      })
+    .pipe(gulp.dest('dist/font'))
+	.on('end', function(){
+		fs.writeFileSync('icon-list.json', JSON.stringify(icons));
+		done();
+	});
 });
+
 gulp.task('default', ['iconfont']);
