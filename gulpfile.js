11cb86294afb771ba46ba473df4f36e7825b2e7d
janteuni <janteuni@student.42.fr>
2015-04-03 15:18:19 +0200

    feat(icon): add first 7 icon file

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,17 @@
 var gulp = require('gulp');
+var iconfont = require('gulp-iconfont');
 
-gulp.task('default', function() {
+gulp.task('iconfont', function(){
+  gulp.src(['icons/*.svg'])
+    .pipe(iconfont({
+      fontName: 'suhicons',
+      appendCodepoints: true
+    }))
+      .on('codepoints', function(codepoints, options) {
+        // CSS templating, e.g.
+        console.log(codepoints, options);
+      })
+    .pipe(gulp.dest('font/'));
 });
+
+gulp.task('default', ['iconfont']);
